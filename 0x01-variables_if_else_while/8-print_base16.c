5dccf66fdea8c49c18cf78a35675e1558a8e80a6
Iwuchukwu Alexander <iwuchukwualexander@gmail.com>
2023-03-17 14:07:15 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints base 16
+ * @void: Empty parameter list for main.
+ * Return: 0 for success
+*/
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+
+	num = 97;
+
+	while (num <= 102)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
