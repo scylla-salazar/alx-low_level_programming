6a70bccec0cfcf31c6c8f9ccd7b72af7703e019d
Iwuchukwu Alexander <iwuchukwualexander@gmail.com>
2023-03-17 13:56:26 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints base 10 numbers
+ * @void: Empty parameter list
+ * Return: 0 for success
+*/
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
