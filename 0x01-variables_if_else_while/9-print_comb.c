141284499a9a1c99d1dcd53fa58d08093dc79f5e
Iwuchukwu Alexander <iwuchukwualexander@gmail.com>
2023-03-17 14:12:12 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - writes 0-9
+ * @void: Empty parameter list for main.
+ * Return: 0 for success
+*/
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		if (num < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
