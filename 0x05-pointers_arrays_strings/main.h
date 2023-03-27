f6bbff357eb05db2072a416fa15a7254679afbeb
Iwuchukwu Alexander <iwuchukwualexander@gmail.com>
2023-03-27 08:02:38 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,30 +1,22 @@
-#include <unistd.h>
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
 /**
- * _putchar - writes the character c to stdout
- * print_alphabet - prints alphabet and returns new line
- * print_alphabet_x10 - print alphabet 10 times
- * _islower - return 1 if c is lowercase, 0 otherwise
- * _isalpha - return 1 if c is upper or lowercase, 0 otherwise
- * print_sign - return 1 for positive, 0 for 0, and -1 for negative
- * _abs - computes absolute value of integer
- * print_last_digit - print last digit
- * add - add two ints, return sum
- * jack_bauer - prints all minutes of the day
- * times_table - prints times table up to 9
- * print_to_98 - prints all natural numbers to 98
- * print_times_table - print times table up to
- **/
-int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int);
-int _isalpha(int);
-int print_sign(int);
-int _abs(int);
-int print_last_digit(int);
-int add(int, int);
-void jack_bauer(void);
-void times_table(void);
-void print_to_98(int);
-void print_times_table(int n);
+ * void prototypes()
+ * int prototypes()
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _putchar(char c);
+
+#endif /* MIAN_H_INCLUDED */
