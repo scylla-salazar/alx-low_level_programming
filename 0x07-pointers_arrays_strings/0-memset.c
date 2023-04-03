1ec257296044aa40f0b8e11239e7af3eeeb565a5
scylla-salazar <125595874+scylla-salazar@users.noreply.github.com>
2023-04-03 18:46:21 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+	unsigned int i;
 
-for (i = 0; n > 0; i++, n--)
-{
-	s[i] = b;
-}
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
 
-return (s);
+	return (s);
 }
