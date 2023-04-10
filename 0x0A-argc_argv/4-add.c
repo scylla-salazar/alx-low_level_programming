1636d440a6b43dab18fac69fe732a1ef2060f26d
Iwuchukwu Alexander <iwuchukwualexander@gmail.com>
2023-04-10 09:23:11 -0700

    a program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,12 +4,10 @@
 
 /**
  * main - add 2 positive numbers and print the result
- *
  * @argc: argument count
  * @argv: argument vector, array of strings
  * Description: If no number is passed to program, print 0.
  * If one of the numbers contain non-digits, print Error.
- * 
  * Return: 1 if error, 0 if function runs properly.
  */
 
