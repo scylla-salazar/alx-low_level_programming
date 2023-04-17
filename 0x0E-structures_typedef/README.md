0f0b51da71e5c087b713fa30f3c86892dc091b46
Iwuchukwu Alexander <iwuchukwualexander@gmail.com>
2023-04-17 12:17:03 -0700

    Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This contains 0x0E-structures_typedef projects
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef FILE_DOG
-#define FILE_DOG
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - structure dog
@@ -24,6 +24,8 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
 
